12a244ddf6285803c6707e93cd9a523d516ecd43
FINDarkside <findarkside@gmail.com>
2019-04-02 07:02:06 +0300

    Implement clearRow

--- src/main/java/tetris/domain/GameBoard.java
+++ src/main/java/tetris/domain/GameBoard.java
@@ -16,6 +16,14 @@ public class GameBoard {
         return board;
     }
 
+    public Block getBlock(int x, int y) {
+        return board[y][x];
+    }
+
+    /**
+     * @param y Target row, 0 is the bottom row
+     * @return Returns true if target row is full
+     */
     public boolean isRowFull(int y) {
         if (y < 0 || y >= height) {
             return false;
@@ -26,5 +34,23 @@ public class GameBoard {
             }
         }
         return true;
+    }
+
+    /**
+     * Removes all blocks from target row. All blocks above the target row will
+     * move one unit down
+     *
+     * @param y Row to clear, 0 is the bottom row
+     */
+    public void clearRow(int y) {
+        for (int x = 0; x < width; x++) {
+            board[y][x] = null;
+        }
+        for (int i = y + 1; i < height; i++) {
+            for (int x = 0; x < width; x++) {
+                board[i - 1][x] = board[i][x];
+                board[i][x] = null;
+            }
+        }
     }
 }
