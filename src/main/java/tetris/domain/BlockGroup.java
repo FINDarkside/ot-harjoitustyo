df46cad9453bbfc7b5e4ba9c20f46ae9e5292c4e
FINDarkside <findarkside@gmail.com>
2019-04-08 17:03:04 +0300

    Fix block group rotation

--- src/main/java/tetris/domain/BlockGroup.java
+++ src/main/java/tetris/domain/BlockGroup.java
@@ -1,50 +1,54 @@
 package tetris.domain;
 
 public class BlockGroup {
-    
+
     private BlockGroupItem[] blocks;
     private int x;
     private int y;
-    
+
     public BlockGroup(BlockGroupItem[] blocks, int x, int y) {
         this.blocks = blocks;
         this.x = x;
         this.y = y;
     }
-    
+
     /**
      * Rotates the block group clockwise
      */
     public void rotateClockwise() {
         for (BlockGroupItem blockItem : blocks) {
-            boolean negativeX = blockItem.getRelativeX() < 0;
-            boolean negativeY = blockItem.getRelativeY() < 0;
-            if (negativeX != negativeY) {
-                blockItem.setRelativeX(-1 * blockItem.getRelativeX());
+            int x = blockItem.getRelativeX();
+            int y = blockItem.getRelativeY();
+            if (x < 0 != y < 0) {
+                x *= -1;
+                x += x < 0 ? 1 : -1;
+                blockItem.setRelativeX(x);
             } else {
-                blockItem.setRelativeY(-1 * blockItem.getRelativeY());
+                y *= -1;
+                y += y < 0 ? 1 : -1;
+                blockItem.setRelativeY(y);
             }
         }
     }
-    
+
     public BlockGroupItem[] getBlocks() {
         return blocks;
     }
-    
+
     public int getX() {
         return x;
     }
-    
+
     public int getY() {
         return y;
     }
-    
+
     public void setX(int x) {
         this.x = x;
     }
-    
+
     public void setY(int y) {
         this.y = y;
     }
-    
+
 }
