0a63f4d407a28f5c7d601cb4beee1bb5b2b56eeb
FINDarkside <findarkside@gmail.com>
2019-04-08 18:01:26 +0300

    Fix multi-block rotation test

--- src/test/java/tetris/domain/BlockGroupTest.java
+++ src/test/java/tetris/domain/BlockGroupTest.java
@@ -28,10 +28,10 @@ public class BlockGroupTest {
     @Test
     public void rotatingMultipleBlocksWork() {
         int[][] expectedResult = new int[][]{
-            new int[]{0, -4, 1 - 3, 2 - 4, 3 - 3},
-            new int[]{-4, -1, -3, -2, -4, -6, -3, -4},
+            new int[]{0, -4, 1, -3, 2, -4, 3, -3},
+            new int[]{-4, -1, -3, -2, -4, -3, -3, -4},
             new int[]{-1, 3, -2, 2, -3, 3, -4, 2},
-            new int[]{3, 0, 2, 1, 3, 2, 2, 4}
+            new int[]{3, 0, 2, 1, 3, 2, 2, 3}
         };
         BlockGroupItem[] groupBlocks = new BlockGroupItem[]{
             new BlockGroupItem(new Block("#000"), 3, 0),
