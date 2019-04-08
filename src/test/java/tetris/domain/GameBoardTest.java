043730f12cb4a24f526fed412b986efcc466f436
FINDarkside <findarkside@gmail.com>
2019-04-08 16:26:05 +0300

    Clean imports, remove license header

--- src/test/java/tetris/domain/GameBoardTest.java
+++ src/test/java/tetris/domain/GameBoardTest.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tetris.domain;
 
 import java.util.Arrays;
-import javafx.scene.paint.Color;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class GameBoardTest {
